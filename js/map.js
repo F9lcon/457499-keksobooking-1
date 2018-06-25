03a339b4ba85dad099b0de3f042f0ff1c54f4aa2
f9lcon <twisy12@mail.ru>
2018-06-25 16:17:14 +0300

    Задание доверяй, но проверяй2

--- js/map.js
+++ js/map.js
@@ -45,6 +45,7 @@ var formElement = document.querySelector('.ad-form');
 var fieldsetElements = formElement.querySelectorAll('fieldset');
 var pinMainElement = document.querySelector('.map__pin--main');
 var inputAddressElement = formElement.querySelector('#address');
+var successElement = document.querySelector('.success');
 
 var getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -172,7 +173,8 @@ var pushCard = function (item) {
 var deactivatePage = function () {
   for (var i = 0; i < fieldsetElements.length; i++) {
     fieldsetElements[i].disabled = true;
-  }
+  };
+
   inputAddressElement.value = (pinMainElement.offsetLeft + MAIN_PIN_WIDTH / 2)
     + ', ' + (pinMainElement.offsetTop - MAIN_PIN_HEIGHT / 2);
 };
@@ -194,6 +196,7 @@ var activatePage = function () {
   pinMainElement.removeEventListener('mouseup', onPinMainElementMouseup);
   pushPins();
   pinsListElement.addEventListener('click', onPinsElementClick);
+  formElement.addEventListener('submit' , onFormSubmit);
 };
 
 var onPinsElementClick = function (evt) {
@@ -228,3 +231,102 @@ var onCloseButtonPressEsc = function (evt) {
     closePopup();
   }
 };
+
+var setMinimalPrice = function () {
+  var inputTypeElement = document.querySelector('#type');
+  var inputPriceElement = document.querySelector('#price');
+  inputTypeElement.addEventListener('change', function () {
+    if (inputTypeElement.value === 'bungalo') {
+      inputPriceElement.min = 0;
+      inputPriceElement.placeholder = 0;
+    } else if (inputTypeElement.value === 'flat') {
+      inputPriceElement.min = 1000;
+      inputPriceElement.placeholder = 1000;
+    } else if (inputTypeElement.value === 'house') {
+      inputPriceElement.min = 5000;
+      inputPriceElement.placeholder = 5000;
+    } else if (inputTypeElement.value === 'palace') {
+      inputPriceElement.min = 10000;
+      inputPriceElement.placeholder = 10000;
+    }
+  });
+};
+
+setMinimalPrice();
+
+var setRoomNubmer = function () {
+  var inputRoomNumberElement = document.querySelector('#room_number');
+  var capacityElement = document.querySelector('#capacity');
+  inputRoomNumberElement.addEventListener('change', function () {
+    if (inputRoomNumberElement.value === '1') {
+      capacityElement[0].disabled = true;
+      capacityElement[1].disabled = true;
+      capacityElement[2].selected = true;
+      capacityElement[3].disabled = true;
+    } else if (inputRoomNumberElement.value === '2') {
+      capacityElement[0].disabled = true;
+      capacityElement[1].disabled = false;
+      capacityElement[2].disabled = false;
+      capacityElement[3].disabled = true;
+      capacityElement[2].selected = true;
+    } else if (inputRoomNumberElement.value === '3') {
+      capacityElement[0].disabled = false;
+      capacityElement[1].disabled = false;
+      capacityElement[2].disabled = false;
+      capacityElement[3].disabled = true;
+      capacityElement[2].selected = true;
+    } else if (inputRoomNumberElement.value === '100') {
+      capacityElement[0].disabled = true;
+      capacityElement[1].disabled = true;
+      capacityElement[2].disabled = true;
+      capacityElement[3].disabled = false;
+      capacityElement[3].selected = true;
+    }
+  });
+};
+setRoomNubmer();
+
+var setTimeInOut = function () {
+  var timeInElement = document.querySelector('#timein');
+  var timeOutElement = document.querySelector('#timeout');
+
+  timeInElement.addEventListener('change', function () {
+    timeOutElement.value = timeInElement.value;
+  });
+
+  timeOutElement.addEventListener('change', function () {
+    timeInElement.value = timeOutElement.value;
+  });
+};
+
+setTimeInOut();
+
+var closeSuccessElement = function () {
+  successElement.classList.add('hidden');
+  document.removeEventListener('keydown', onSuccessPressEsc);
+};
+
+var onSuccessPressEsc = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeSuccessElement();
+    formElement.reset();
+    mapElement.classList.add('map--faded');
+    formElement.classList.add('ad-form--disabled');
+    for (var i = pinsListElement.children.length; i > 1; i--) {
+      pinsListElement.removeChild(pinsListElement.lastChild);
+    }
+    pinMainElement.addEventListener('mouseup', onPinMainElementMouseup);
+  }
+};
+
+var renderSuccess = function () {
+  successElement.classList.remove('hidden');
+  document.addEventListener('keydown', onSuccessPressEsc);
+};
+
+var onFormSubmit = function (evt) {
+  evt.preventDefault();
+  renderSuccess();
+};
+
+
